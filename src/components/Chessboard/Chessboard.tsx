e34ddea5f34712bcaa7e6a93a5e98ab33351833f
James Kwon <jhk1097@gmail.com>
2024-06-23 23:03:14 +1200

    Styling pieces

--- src/components/Chessboard/Chessboard.tsx
+++ src/components/Chessboard/Chessboard.tsx
@@ -5,14 +5,32 @@ import Tile from '../Tile/Tile'
 const verticalAxis = ['1', '2', '3', '4', '5', '6', '7', '8']
 const horizontalAxis = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
 
+interface Piece {
+  image: string
+  x: number
+  y: number
+}
+
+const pieces: Piece[] = []
+
+for (let i = 0; i < 8; i++)
+  pieces.push({ image: 'assets/images/blackpawn.png', x: i, y: 6 })
+
 export default function Chessboard() {
   let board = []
 
   for (let j = verticalAxis.length - 1; j >= 0; j--) {
     for (let i = 0; i < horizontalAxis.length; i++) {
       const number = j + i + 2
+      let image = undefined
 
-      board.push(<Tile number={number} />)
+      pieces.forEach((p) => {
+        if (p.x === i && p.y === j) {
+          image = p.image
+        }
+      })
+
+      board.push(<Tile image={image} number={number} />)
     }
   }
   return <div id="chessboard">{board}</div>
--- src/components/Tile/Tile.tsx
+++ src/components/Tile/Tile.tsx
@@ -1,17 +1,22 @@
 import './Tile.css'
 
 interface Props {
+  image?: string
   number: number
 }
 
-export default function Tile({ number }: Props) {
+export default function Tile({ number, image }: Props) {
   if (number % 2 === 0) {
     return (
       <div className="tile black-tile">
-        <img src="./assets/images/blackpawn.png" alt="Black pawn" />
+        <img src={image} />
       </div>
     )
   } else {
-    return <div className="tile white-tile"></div>
+    return (
+      <div className="tile white-tile">
+        <img src={image} />
+      </div>
+    )
   }
 }
