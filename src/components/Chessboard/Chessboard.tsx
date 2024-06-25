de050800e30bd2f469ccad44c814934b4e19e5b8
James Kwon <jhk1097@gmail.com>
2024-06-25 12:20:00 +1200

    Added a key to fix the key warning

--- src/components/Chessboard/Chessboard.tsx
+++ src/components/Chessboard/Chessboard.tsx
@@ -47,7 +47,7 @@ export default function Chessboard() {
         }
       })
 
-      board.push(<Tile image={image} number={number} />)
+      board.push(<Tile key={`${j},${i}`} image={image} number={number} />)
     }
   }
   return <div id="chessboard">{board}</div>
