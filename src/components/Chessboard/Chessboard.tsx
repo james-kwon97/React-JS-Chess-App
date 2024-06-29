be4160f489620ae0e6c8550e1d38e7bea23910d1
James Kwon <jhk1097@gmail.com>
2024-06-29 19:29:16 +1200

    Adding PieceType to piece interface

--- src/components/Chessboard/Chessboard.tsx
+++ src/components/Chessboard/Chessboard.tsx
@@ -10,6 +10,7 @@ interface Piece {
   image: string
   x: number
   y: number
+  type: PieceType
 }
 
 export enum PieceType {
@@ -27,22 +28,72 @@ for (let p = 0; p < 2; p++) {
   const type = p === 0 ? 'black' : 'white'
   const y = p === 0 ? 7 : 0
 
-  initialBoardState.push({ image: `assets/images/${type}rook.png`, x: 0, y })
-  initialBoardState.push({ image: `assets/images/${type}rook.png`, x: 7, y })
-  initialBoardState.push({ image: `assets/images/${type}knight.png`, x: 1, y })
-  initialBoardState.push({ image: `assets/images/${type}knight.png`, x: 6, y })
-  initialBoardState.push({ image: `assets/images/${type}bishop.png`, x: 2, y })
-  initialBoardState.push({ image: `assets/images/${type}bishop.png`, x: 5, y })
-  initialBoardState.push({ image: `assets/images/${type}queen.png`, x: 3, y })
-  initialBoardState.push({ image: `assets/images/${type}king.png`, x: 4, y })
+  initialBoardState.push({
+    image: `assets/images/${type}rook.png`,
+    x: 0,
+    y,
+    type: PieceType.ROOK,
+  })
+  initialBoardState.push({
+    image: `assets/images/${type}rook.png`,
+    x: 7,
+    y,
+    type: PieceType.ROOK,
+  })
+  initialBoardState.push({
+    image: `assets/images/${type}knight.png`,
+    x: 1,
+    y,
+    type: PieceType.KNIGHT,
+  })
+  initialBoardState.push({
+    image: `assets/images/${type}knight.png`,
+    x: 6,
+    y,
+    type: PieceType.KNIGHT,
+  })
+  initialBoardState.push({
+    image: `assets/images/${type}bishop.png`,
+    x: 2,
+    y,
+    type: PieceType.BISHOP,
+  })
+  initialBoardState.push({
+    image: `assets/images/${type}bishop.png`,
+    x: 5,
+    y,
+    type: PieceType.BISHOP,
+  })
+  initialBoardState.push({
+    image: `assets/images/${type}queen.png`,
+    x: 3,
+    y,
+    type: PieceType.QUEEN,
+  })
+  initialBoardState.push({
+    image: `assets/images/${type}king.png`,
+    x: 4,
+    y,
+    type: PieceType.KING,
+  })
 }
 
 for (let i = 0; i < 8; i++) {
-  initialBoardState.push({ image: 'assets/images/blackpawn.png', x: i, y: 6 })
+  initialBoardState.push({
+    image: 'assets/images/blackpawn.png',
+    x: i,
+    y: 6,
+    type: PieceType.PAWN,
+  })
 }
 
 for (let i = 0; i < 8; i++) {
-  initialBoardState.push({ image: 'assets/images/whitepawn.png', x: i, y: 1 })
+  initialBoardState.push({
+    image: 'assets/images/whitepawn.png',
+    x: i,
+    y: 1,
+    type: PieceType.PAWN,
+  })
 }
 
 export default function Chessboard() {
@@ -116,11 +167,12 @@ export default function Chessboard() {
       const y = Math.abs(
         Math.ceil((e.clientY - chessboard.offsetTop - 640) / 80)
       )
-      referee.isValidMove()
 
+      // UPDATES THE PIECE POSITION
       setPieces((value) => {
         const pieces = value.map((p) => {
           if (p.x === gridX && p.y === gridY) {
+            referee.isValidMove(gridX, gridY, x, y, p.type)
             p.x = x
             p.y = y
           }
