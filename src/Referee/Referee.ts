de3e5a65d2c41c72d67a0463af62b545fa33934c
James Kwon <jhk1097@gmail.com>
2024-06-29 19:16:49 +1200

    Adding parameters for the referee function

--- src/Referee/Referee.ts
+++ src/Referee/Referee.ts
@@ -1,5 +1,5 @@
 export default class Referee {
-  isValidMove() {
+  isValidMove(px: number, py: number, x: number, y: number, type) {
     console.log('Referee is checking  the move..')
     return true
   }
