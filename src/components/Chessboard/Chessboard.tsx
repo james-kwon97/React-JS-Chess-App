98d5fbe25d18e8319282054edd64f85c7ff6fc57
James Kwon <jhk1097@gmail.com>
2024-07-06 22:05:39 +1200

    Updating en passant check

--- src/components/Chessboard/Chessboard.tsx
+++ src/components/Chessboard/Chessboard.tsx
@@ -131,16 +131,11 @@ export default function Chessboard() {
           // AND IF A PIECE IS ATTACKED, REMOVES IT
           const updatedPieces = pieces.reduce((results, piece) => {
             if (samePosition(piece.position, grabPosition)) {
-              if (
+              // SPECIAL MOVE
+              piece.enPassant =
                 Math.abs(grabPosition.y - y) === 2 &&
                 piece.type === PieceType.PAWN
-              ) {
-                // SPECIAL MOVE
 
-                piece.enPassant = true
-              } else {
-                piece.enPassant = false
-              }
               piece.position.x = x
               piece.position.y = y
               results.push(piece)
