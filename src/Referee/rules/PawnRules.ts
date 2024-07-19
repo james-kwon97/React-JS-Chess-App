a03edebadebd2a118da8ed73d4398da5a352261f
James Kwon <jhk1097@gmail.com>
2024-07-19 16:55:46 +1200

    Adding the 2-tile move as a possible move for the pawn's first move

--- src/Referee/rules/PawnRules.ts
+++ src/Referee/rules/PawnRules.ts
@@ -62,6 +62,7 @@ export const GetPossiblePawnMoves = (
 ): Position[] => {
   const possibleMoves: Position[] = []
 
+  const specialRow = pawn.team === TeamType.OUR ? 1 : 6
   const pawnDirection = pawn.team === TeamType.OUR ? 1 : -1
 
   if (
@@ -69,10 +70,23 @@ export const GetPossiblePawnMoves = (
       { x: pawn.position.x, y: pawn.position.y + pawnDirection },
       boardState
     )
-  )
+  ) {
     possibleMoves.push({
       x: pawn.position.x,
       y: pawn.position.y + pawnDirection,
     })
+    if (
+      pawn.position.y === specialRow &&
+      !tileIsOccupied(
+        { x: pawn.position.x, y: pawn.position.y + pawnDirection * 2 },
+        boardState
+      )
+    ) {
+      possibleMoves.push({
+        x: pawn.position.x,
+        y: pawn.position.y + pawnDirection * 2,
+      })
+    }
+  }
   return possibleMoves
 }
