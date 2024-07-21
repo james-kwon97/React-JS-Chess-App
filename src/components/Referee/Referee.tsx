0386f8bdc24344aec6456f38c38d37dca962aac8
James Kwon <jhk1097@gmail.com>
2024-07-21 23:19:35 +1200

    Adding referee functions

--- src/components/Referee/Referee.tsx
+++ src/components/Referee/Referee.tsx
@@ -1,6 +1,12 @@
+import { Position } from '../../Constants'
 import Chessboard from '../Chessboard/Chessboard'
 
 export default function Referee() {
+  function getPossibleMoves(): Position[] {
+    return []
+  }
+
+  function playMove() {}
   return (
     <>
       <Chessboard />
