7196dead83b9e8a28c14ff6a551fd808dda7ebf4
James Kwon <jhk1097@gmail.com>
2024-07-27 20:24:38 +1200

    Adding chess piece functions to isPieceType logic to Piece

--- src/models/Piece.ts
+++ src/models/Piece.ts
@@ -15,6 +15,30 @@ export class Piece {
     this.team = team
   }
 
+  isPawn(): boolean {
+    return this.type === PieceType.PAWN
+  }
+
+  isRook(): boolean {
+    return this.type === PieceType.ROOK
+  }
+
+  isKnight(): boolean {
+    return this.type === PieceType.KNIGHT
+  }
+
+  isBishop(): boolean {
+    return this.type === PieceType.BISHOP
+  }
+
+  isKing(): boolean {
+    return this.type === PieceType.KING
+  }
+
+  isQueen(): boolean {
+    return this.type === PieceType.QUEEN
+  }
+
   samePiecePosition(otherPiece: Piece): boolean {
     return this.position.samePosition(otherPiece.position)
   }
