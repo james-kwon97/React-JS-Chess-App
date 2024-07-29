f854481c10d35db0044a57be36b4990dd02fee35
James Kwon <jhk1097@gmail.com>
2024-07-29 23:55:57 +1200

    Fixing the enPassant move not working error

--- src/models/Pawn.ts
+++ src/models/Pawn.ts
@@ -4,7 +4,11 @@ import { Position } from './Position'
 
 export class Pawn extends Piece {
   enPassant?: boolean
-  constructor(position: Position, team: TeamType) {
+  constructor(position: Position, team: TeamType, enPassant?: boolean) {
     super(position, PieceType.PAWN, team)
+    this.enPassant = enPassant
+  }
+  clone(): Pawn {
+    return new Pawn(this.position.clone(), this.team, this.enPassant)
   }
 }
