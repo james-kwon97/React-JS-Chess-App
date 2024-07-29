606cfbb6f20cfe00caa55763efad775f0ada0f11
James Kwon <jhk1097@gmail.com>
2024-07-29 22:59:50 +1200

    Adding possibleMoves into our constructor

--- src/models/Piece.ts
+++ src/models/Piece.ts
@@ -7,11 +7,17 @@ export class Piece {
   type: PieceType
   team: TeamType
   possibleMoves?: Position[]
-  constructor(position: Position, type: PieceType, team: TeamType) {
+  constructor(
+    position: Position,
+    type: PieceType,
+    team: TeamType,
+    possibleMoves: Position[] = []
+  ) {
     this.image = `assets/images/${team}-${type}.png`
     this.position = position
     this.type = type
     this.team = team
+    this.possibleMoves = possibleMoves
   }
 
   get isPawn(): boolean {
